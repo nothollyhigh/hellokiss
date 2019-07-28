ab217633c335421065ae4dd70455132d06c8d0da
nothollyhigh <174077440@qq.com>
2019-07-28 15:37:13 +0800

    - support websocket binary message gzip

--- client/assets/Script/Websocket.js
+++ client/assets/Script/Websocket.js
@@ -82,43 +82,54 @@ class Websocket{
     }
 
     onTextMessage(self, event) {
-        var self = this;
         try {
-            var arr = new TextEncoder("utf-8").encode(event.data.slice(0,16));
-            var length = arr[0] | arr[1]<<8 | arr[2]<<16 | arr[3]<<24;
-            var cmd = arr[4] | arr[5]<<8 | arr[6]<<16 | arr[7]<<24;
+            var headArr = new TextEncoder("utf-8").encode(event.data.slice(0,16));
+            var bodyArr = event.data.slice(16, event.data.length);
+            var length = headArr[0] | headArr[1]<<8 | headArr[2]<<16 | headArr[3]<<24;
+            var cmd = headArr[4] | headArr[5]<<8 | headArr[6]<<16 | headArr[7]<<24;
+            var compressedFlag = cmd & self.CmdFlagMaskGzip;
+            // text message does not support compress
+            // if (compressedFlag == self.CmdFlagMaskGzip) {
+            //     cmd = cmd & (~self.CmdFlagMaskGzip);
+            //     cc.log("--- 333 compressedFlag: ", cmd, compressedFlag, self.CmdFlagMaskGzip, bodyArr)
+            //     bodyArr = pako.ungzip(bodyArr, { to: 'string' })
+            // }
             if (cmd == self.CmdPing) { return };
             var data;
             if (event.data.length > 16) {
-                data = JSON.parse(event.data.slice(16, event.data.length));
+                data = JSON.parse(bodyArr);
             }
-            // cc.log("onmessage data: ", data);
+            
+            cc.log("onTextMessage cmd:", cmd, "data:", data);
+
             self.onMessage(cmd, data)
         } catch(e) {
-            cc.log("Websocket onmessage panic:", e);
+            cc.log("Websocket onTextMessage panic:", e);
         }
     }
 
     onBinaryMessage(self, event) {
         try {
             var headArr = new Uint8Array(event.data.slice(0, 16));
-            var bodyArr = new TextDecoder("utf-8").decode(event.data.slice(16, event.data.length));
+            var bodyArr = new Uint8Array(event.data.slice(16, event.data.length));
             var length = headArr[0] | headArr[1]<<8 | headArr[2]<<16 | headArr[3]<<24;
             var cmd = headArr[4] | headArr[5]<<8 | headArr[6]<<16 | headArr[7]<<24;
-            // console.log("--- 111 cmd: ", cmd, self.CmdFlagMaskGzip, cmd & self.CmdFlagMaskGzip, cmd & self.CmdFlagMaskGzip == self.CmdFlagMaskGzip)
-            if ((cmd & self.CmdFlagMaskGzip) == self.CmdFlagMaskGzip) {
-                cmd = (cmd << 1) >> 1;
-                // console.log("--- 222 cmd: ", cmd)
-                bodyArr = pako.inflate(bodyArr);
+            var compressedFlag = cmd & self.CmdFlagMaskGzip;
+            if (compressedFlag == self.CmdFlagMaskGzip) {
+                cmd = cmd & (~self.CmdFlagMaskGzip);
+                bodyArr = pako.ungzip(bodyArr, { to: 'string' })
             }
             if (cmd == self.CmdPing) { return };
             var data;
             if (length > 0 && bodyArr.length > 16) {
                 data = JSON.parse(bodyArr);
             }
+
+            cc.log("onBinaryMessage cmd:", cmd, "data:", data);
+
             self.onMessage(cmd, data)
         } catch(e) {
-            cc.log("Websocket onmessage panic:", e);
+            cc.log("Websocket onBinaryMessage panic:", e);
         }
     }
 
--- server/app/wsserver.go
+++ server/app/wsserver.go
@@ -16,7 +16,7 @@ var (
 
 func startWsServer() {
 	var err error
-	var cipher = net.NewCipherGzip(-1)
+	var cipher = net.NewCipherGzip(net.CipherGzipAll)
 
 	wsServer, err = net.NewWebsocketServer("KISS", config.SvrAddr)
 	if err != nil {
